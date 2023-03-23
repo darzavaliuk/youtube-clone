9c99db231a0ee6cbc921aab142717f36877a81f0
Daria <dar.zavaliuk@gmail.com>
2023-03-24 02:40:17 +0300

    feat: add main class

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+// import { Feed } from '@mui/icons-material';
+import { NavBar, Feed, VideoDetail, SearchFeed, ChannelDetail } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Box sx={{ backgroundColor: '#000' }}>
+          <NavBar />
+          <Routes>
+            <Route exact path='/' element={<Feed />} />
+            <Route path='/video/:id' element={<VideoDetail />} />
+            <Route path='/channel/:id' element={<ChannelDetail />} />
+            <Route path='/search/:searchTerm' element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
