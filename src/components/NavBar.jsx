517106012557b95b4df52a5549b91285bedac7de
Daria <dar.zavaliuk@gmail.com>
2023-05-20 22:07:49 +0300

    fix: change routes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import { SearchBar } from "./";
 
 const Navbar = () => (
     <Stack direction="row" alignItems="center" p={1} sx={{ position: "sticky", background: '#000', top: 7, justifyContent: "space-between" }}>
-        <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <Link to="/youtube-clone" style={{ display: "flex", alignItems: "center" }}>
             <img src={logo} alt="logo" height={35}  />
         </Link>
         <SearchBar />
