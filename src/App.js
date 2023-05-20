41cbbb48ed6b06c0a8a20ba66f7b05ac8600023c
Daria <dar.zavaliuk@gmail.com>
2023-05-20 22:00:43 +0300

    Your commit message

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Box sx={{ backgroundColor: '#000' }}>
           <NavBar />
           <Routes>
-            <Route exact path='/' element={<Feed />} />
+            <Route exact path='/youtube-clone' element={<Feed />} />
             <Route path='/video/:id' element={<VideoDetail />} />
             <Route path='/channel/:id' element={<ChannelDetail />} />
             <Route path='/search/:searchTerm' element={<SearchFeed />} />
